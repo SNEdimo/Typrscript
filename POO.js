e6b74c110774e6b602b3b33da64e7070517d374b
SN Edimo <167471710+SNEdimo@users.noreply.github.com>
2024-05-24 08:06:44 -0300

    Update POO.js

--- POO.js
+++ POO.js
@@ -1,12 +1,28 @@
 function Pessoa(nome, idade){
     this.nome = nome
     this.idade = idade
-
-    Dados(nome, idade)
 }
 
-function Dados(nome, idade){
-    console.log(`Nome: ${nome}\nIdade: ${idade}`)
+Pessoa.prototype.dados = function() {
+    console.log(`Nome: ${this.nome}\nIdade: ${this.idade}`)
 }
 
 const p = new Pessoa("Felps", 21)
+const p1 = new Pessoa("Amanda", 34)
+
+p.dados()
+p1.dados()
+
+
+function Empregado(nome, idade, cargo){
+    Pessoa.call(this, nome, idade)
+    this.cargo = cargo
+}
+Empregado.prototype
+Empregado.prototype = Object.create(Pessoa.prototype)
+
+
+const em = new Empregado("Jorge", 44, "Empresario")
+
+console.log(em.cargo)
+em.dados()
